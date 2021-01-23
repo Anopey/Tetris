870bcd6cdf744c84c3a3ac09a1e0882638bf7fc4
Ozan Ürkmez <arkantos250@gmail.com>
2021-01-23 15:49:22 +0300

    rotation function

--- Tetris/Source.cpp
+++ Tetris/Source.cpp
@@ -10,6 +10,8 @@ string tetromino[noTetromino];
 
 void PopulateAssets();
 
+int Rotate();
+
 int main() {
 
 	PopulateAssets();
@@ -36,4 +38,13 @@ void PopulateAssets() {
 
 	}
 
+}
+
+int Rotate(int px, int py, int r) {
+	switch (r % 4) {
+	case 0: return py * 4 + px;			// 0 degrees
+	case 1: return 12 + py - (px * 4);	// 90 degrees
+	case 2: return 15 - (py * 4) - px;	// 180 degrees
+	case 3: return 3 - py + (px * 4);	// 270 degrees
+	}
 }
